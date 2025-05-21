a3b187d61cabd2ccff4e6b87c9d6f6be9a38cf3b
med-reda-nk <medredank@gmail.com>
2025-05-21 15:33:07 +0100

    Create conf.py

---
+++ Documentation/conf.py
@@ -0,0 +1,60 @@
+# conf.py
+
+# -- General configuration ------------------------------------------------
+
+extensions = [
+    "sphinx_rtd_theme",  # Ajout du thème ReadTheDocs
+]
+
+# The master toctree document.
+master_doc = 'index'
+
+# Informations générales
+project = 'M-eco'
+copyright = '2025, Mohamed Reda Nkira - Zouga Mouhcine'
+author = 'Mohamed Reda Nkira - Zouga Mouhcine'
+
+version = '0.1'
+release = '0.1.0'
+
+language = 'fr'  # Spécifie le français pour la génération de contenu
+
+# -- Options pour le rendu HTML -------------------------------------------
+
+html_theme = 'sphinx_rtd_theme'
+html_theme_options = {
+    'collapse_navigation': False,
+    'navigation_depth': 3,
+    'style_external_links': True,
+}
+
+html_static_path = ['_static']  # Assure-toi que ce dossier existe ou enlève cette ligne
+htmlhelp_basename = 'M-ecoDoc'
+
+# -- Options pour le rendu LaTeX ------------------------------------------
+
+latex_elements = {
+    'papersize': 'a4paper',
+    'pointsize': '11pt',
+    'preamble': '',
+}
+
+latex_documents = [
+    (master_doc, 'M-eco.tex', 'Documentation GuardVision',
+     author, 'manual'),
+]
+
+# -- Options pour le rendu man --------------------------------------------
+
+man_pages = [
+    (master_doc, 'M-eco', 'Documentation GuardVision',
+     [author], 1)
+]
+
+# -- Options pour le rendu Texinfo ----------------------------------------
+
+texinfo_documents = [
+    (master_doc, 'M-eco', 'Documentation GuardVision',
+     author, 'GuardVision', 'Système de prédiction des indicateurs economiques du Maroc.',
+     'Miscellaneous'),
+]
